e1c5cd6c2a7a1781bde70abd0357924c63eaa48a
Barryk112 <barrykiely112@gmail.com>
2023-05-29 16:05:30 +0000

    Completed homepage header and CSS

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), # URLS for allauth login ect..
+    path('accounts/', include('allauth.urls')),  # URLS for allauth login ect..
     path('', include('home.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Link for the media
