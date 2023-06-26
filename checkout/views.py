8151a25355cf759dc0050cdda7e6e39766b95623
Barryk112 <barrykiely112@gmail.com>
2023-06-26 20:41:33 +0000

    Added env keys to gitpod restarting workspace

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from bag.context import bag_contents
+from django.conf import settings
+
+import stripe
 
 from .forms import OrderForm
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         message.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currancy=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
