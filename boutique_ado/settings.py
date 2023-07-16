a8828d62d4971529c470b3a52ba4c4cd249067f0
Barryk112 <barrykiely112@gmail.com>
2023-07-16 14:53:34 +0000

    Fixed typo in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -184,7 +184,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Staic and mdeia files
-    STATICFILES_STORAGE = 'custom_storage.StaicStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaicStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
