97cc2dc6ea1e60b9b64e63b18ea03663b50dafb9
Barryk112 <barrykiely112@gmail.com>
2023-07-18 19:03:24 +0000

    Updated settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -213,3 +213,10 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
